9e2dc9408011fdf4c2369d52abbb603a9bf4482c
AngelaD <55364919+jna21@users.noreply.github.com>
2020-03-31 07:57:01 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,72 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * `quotes` array 
+***/
+
+let quoteList = [
+  {quote: "You are the shuckiest shuck faced shuck in the world!",
+  source: "James Dashner",
+  citation: "The Maze Runner 1",
+  year: 2009
+  },
+  {quote: "Insane means fewer cameras!",
+  source: "Ally Carter",
+  citation: "Only the Good Spy Young",
+  year: 2010
+  },
+  {quote: "Tried to escape, to block out the fact that I was being eaten alive by arachnids. For some reason the only thing I could replace it with was the image of being eaten by tiny clowns.",
+  source: "David Wong",
+  citation: "This Book is Full of Spiders",
+  year: 2012
+  },
+  {quote: "I said 'I'm sorry, sir, but we don't speak Swedish.' 'Well, of course you don't. Neither do I. Who the hell speaks Swedish?",
+  source: "John Green Fank",
+  citation: "The Fault in Our Stars",
+  year: 2012
+  },
+  {quote: "Entropy is just a fancy way of saying: things fall apart.",
+  source: "Dan Brown",
+  citation: "Origin",
+  year: 2017
+  },
+  {quote: "I love “Sometimes we know people who are too wonderful for words. I am not one of them. Or you, for that matter, as you well know.",
+  source: "Michael Hogan",
+  citation: "Winter Solstice",
+  year: 2011
+  },
+]
+
+
+
+/***
+ * `getRandomQuote` function
+***/
+
+function getRandomQuote {
+
+};
+
+/***
+ * `printQuote` function
+***/
+
+
+
+function printQuote {
+    
+}
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
