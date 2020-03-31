cec68d1c019163ac4e5a25047e4ef5377547348e
AngelaD <55364919+jna21@users.noreply.github.com>
2020-03-31 13:48:47 -0500

    Added JS functions for project first round

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ let quoteList = [
   citation: "This Book is Full of Spiders",
   year: 2012
   },
-  {quote: "I said 'I'm sorry, sir, but we don't speak Swedish.' 'Well, of course you don't. Neither do I. Who the hell speaks Swedish?",
+  {quote: "I said I'm sorry, sir, but we don't speak Swedish. Well, of course you don't. Neither do I. Who the hell speaks Swedish?",
   source: "John Green Fank",
   citation: "The Fault in Our Stars",
   year: 2012
@@ -37,32 +37,78 @@ let quoteList = [
   citation: "Origin",
   year: 2017
   },
-  {quote: "I love “Sometimes we know people who are too wonderful for words. I am not one of them. Or you, for that matter, as you well know.",
+  {quote: "Sometimes we know people who are too wonderful for words. I am not one of them. Or you, for that matter, as you well know.",
   source: "Michael Hogan",
   citation: "Winter Solstice",
   year: 2011
   },
+  {quote:"The greatest discovery of all time is that a person can change his future by merely changing his attitude.",
+  source: "Oprah Winfrey"
+  }
 ]
 
-
-
+// console.log(` Student info print test. her is the Quote "${quoteList[0].quote}"  Here is the book its from ${quoteList[0].citation} Here is the source name and the year ${quoteList[0].source} ${quoteList[0].year}`)
 /***
  * `getRandomQuote` function
 ***/
-
-function getRandomQuote {
-
+let chosenQuote;
+function getRandomQuote(quoteArray) {
+  //create a variable that generates a random number between 0 and the last index of the quote array
+  //first use Math.random to get a number within the length of the array of quotes
+  var numberPicked = Math.floor(Math.random() * (quoteArray.length + 1) );
+  //Use the random number variable and bracket notation to grab a random object from the `quotes` array, and store it in a variable
+     //test the numberPicked to make sure its working properly using console.log - passed
+     //console.log("Number Picked: " + numberPicked)
+  chosenQuote = quoteArray[numberPicked];
+      //test chosenQuote Variable using console.log - passed
+      //console.log("Chosen Quote Object is: " + chosenQuote)
+  // Return the variable storing the random quote object
+  return chosenQuote;
 };
+//getRandomQuote(quoteList);
+  //test function to make sure it produces the random object using console.log  - passed
+  // console.log(getRandomQuote(quoteList) );
 
-/***
- * `printQuote` function
-***/
+// /***
+//  * `printQuote` function
+// ***/
 
-
-
-function printQuote {
+let quoteString = '';
+  function printQuote(quotesArr) {
+    // 1. Create a variable that calls the getRandomQuote() 
+    // function
+    let callRandom = getRandomQuote(quotesArr);
+    // 2. Create a variable that initiates your HTML string with 
+    // the first two <p></p> elements, their classNames, 
+    // and the quote and source properties, but leave off 
+    // the second closing `</p>` tag for now
+    quoteString += `<p class="quote"> ${callRandom.quote} </p>`;
+    console.log
+       // console.log(quoteString) Tested Quote String with console.log - passed;
+    quoteString += `<p class="source"> ${callRandom.source}`;
+       // console.log(quoteString) Tested Quote String with console.log - passed;
+    // 3. Use an if statement to check if the citation property  exists, and if it does, concatenate a <span></span> element, appropriate className, and citation property  to the HTML string
+      if (callRandom.citation) {
+        quoteString +=  `<span class="citation"> ${callRandom.citation} </span>`;
+      }
+    // 4. Use an if statement to check of the year property exists, and if it does, concatenate a <span></span> element, appropriate className, and year property to the HTML 
+    //string
+    if (callRandom.year) {
+      quoteString +=  `<span class = "year"> ${callRandom.year} </span>`;
+    }
+          //test to see if quoteString works with missig elements using console.log - passed;
+        //console.log(quoteString); 
+  
+    // 5. After the two if statements, concatenate the closing </p>  tag to the HTML string
+    quoteString += '</p>';
+    console.log("Current: " + quoteString);
+  
+    // 6. set the innerHTML of the quote-box div to equal the complete HTML string
+    document.getElementById('quote-box').innerHTML = quoteString; 
+    return quoteString;
     
-}
+  }
+  printQuote(quoteList);
 
 /***
  * click event listener for the print quote button
