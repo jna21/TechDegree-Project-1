93da87b3951f6050ef91558538b94150343e60d7
AngelaD <55364919+jna21@users.noreply.github.com>
2020-04-01 19:47:14 -0500

    Added final Extra Credit Functions
    Added a separate .js file to hold the two additional functions to change the background color and then change quotes at a given interval.

---
+++ js/extraCredit.js
@@ -0,0 +1,25 @@
+//EXTRA CREDIT #1: was added onto the printQuote function int he script.js file
+
+// EXTRA CREDIT #2: added function to change background color on click of button
+function changeBkgdColor () {
+  //watched youTube video to learn how to do change a background color randomly: https://youtu.be/xF-xR22oEbY
+  //used Math.floor(Math.random) from a range of 0 - 256 to generage each of the colors for RGB color set up
+  let x = Math.floor(Math.random() * 256);
+  let y = Math.floor(Math.random() * 256);
+  let z = Math.floor(Math.random() * 256);
+  //created a variable string for the new RGB color for the body color
+  let newBkgdColor = "rgb(" + x + "," + y + "," + z + ")" 
+  //applied the new color to the specific CSS Id element for the body background color;
+  document.body.style.backgroundColor = newBkgdColor;
+  //tested using console.log to make sure changeBkgdcolor creates random RGB colors - passed.
+  // console.log(newBkgdColor);
+}
+document.getElementById('load-quote').addEventListener("click", changeBkgdColor, false);
+//adding an event listenter to change the color of the background on the page on with new changeBackground function when button clicked
+
+//EXTRA CREDIT #3: added a timing function to make the quote automatically refresh every 20 seconds(20,000 miliseconds) using setInterval() calling on the printQuote function
+//referenced video on YouTube to learn how to use setInterval: https://youtu.be/CqDqHiamRHA  
+function quoteRefresh() {
+    setInterval(printQuote, 5000);
+}
+quoteRefresh();
--- js/script.js
+++ js/script.js
@@ -15,7 +15,8 @@ let quotes = [
   {quote: "You are the shuckiest shuck faced shuck in the world!",
   source: "James Dashner",
   citation: "The Maze Runner 1",
-  year: 2009
+  year: 2009,
+  genre: "Adventure"
   },
   {quote: "Insane means fewer cameras!",
   source: "Ally Carter",
@@ -25,7 +26,8 @@ let quotes = [
   {quote: "Tried to escape, to block out the fact that I was being eaten alive by arachnids. For some reason the only thing I could replace it with was the image of being eaten by tiny clowns.",
   source: "David Wong",
   citation: "This Book is Full of Spiders",
-  year: 2012
+  year: 2012,
+  genre: "Horror"
   },
   {quote: "I said I'm sorry, sir, but we don't speak Swedish. Well, of course you don't. Neither do I. Who the hell speaks Swedish?",
   source: "John Green Fank",
@@ -35,7 +37,8 @@ let quotes = [
   {quote: "Entropy is just a fancy way of saying: things fall apart.",
   source: "Dan Brown",
   citation: "Origin",
-  year: 2017
+  year: 2017,
+  genre: "Suspense"
   },
   {quote: "Sometimes we know people who are too wonderful for words. I am not one of them. Or you, for that matter, as you well know.",
   source: "Michael Hogan",
@@ -64,8 +67,8 @@ function getRandomQuote(quoteArray) {
   // Return the variable storing the random quote object
   return chosenQuote;
 };
-//getRandomQuote(quotes);
-  //test function to make sure it produces the random object using console.log  - passed
+getRandomQuote(quotes);
+  // test function to make sure it produces the random object using console.log  - passed
   // console.log(getRandomQuote(quotes) );
 
 // /***
@@ -73,55 +76,51 @@ function getRandomQuote(quoteArray) {
 // ***/
 
 
-  function printQuote() {
-    //Created a variable calling getRandomQuote() function on input array
-    let callRandom = getRandomQuote(quotes);
-    // Created variable to begin my  HTML quote string with "quote" class for callRandom.quote
-    let quoteString = `<p class="quote"> ${callRandom.quote}</p>`;
-       // console.log(quoteString) Tested Quote String with console.log - passed;
-    quoteString += `<p class="source"> ${callRandom.source}`;
-       // console.log(quoteString) Tested Quote String with console.log - passed;
-    // Used if statement to see if 'citation' property is present - if true add 'callRandom.citation' to quoteString  with a class of 'span'     
-    if (callRandom.citation) {
-        quoteString +=  `<span class="citation"> ${callRandom.citation} </span>`;
-      }
-    // Used if statement to see if 'year' property is present - if true add 'callRandom.year' to quoteString  with a class of 'span' 
-    if (callRandom.year) {
-      quoteString +=  `<span class = "year"> ${callRandom.year} </span>`;
-    }
-          //test to see if quoteString works with missig elements using console.log - passed;
+function printQuote() {
+  //Created a variable calling getRandomQuote() function on input array
+  let callRandom = getRandomQuote(quotes);
+  // Created variable to begin my  HTML quote string with "quote" class for callRandom.quote
+  let quoteString = `<p class="quote"> ${callRandom.quote}</p>`;
+     // console.log(quoteString) Tested Quote String with console.log - passed;
+  quoteString += `<p class="source"> ${callRandom.source}`;
+     // console.log(quoteString) Tested Quote String with console.log - passed;
+  // Used if statement to see if 'citation' property is present - if true add 'callRandom.citation' to quoteString  with a class of 'span'     
+  if (callRandom.citation) {
+    quoteString +=  `<span class="citation"> ${callRandom.citation} </span>`;
+  }
+  // Used if statement to see if 'year' property is present - if true add 'callRandom.year' to quoteString  with a class of 'span' 
+  if (callRandom.year) {
+    quoteString +=  `<span class = "year"> ${callRandom.year} </span>`;
+  }
+      //test to see if quoteString works with missig elements using console.log - passed;
         //console.log(quoteString); 
+
+    //EXTRA CREDIT #1  - added a Genre to the quote string
+  if (callRandom.genre) {
+    quoteString +=  `<span class ="genre"> ${callRandom.genre} </span>`;
+    }
+      //test to see if quoteString works with missig elements using console.log - passed;
+        // console.log(quoteString); 
   
-    //  concatenate the closing </p>  tag to end of string
-    quoteString += '</p>';
+  //  concatenate the closing </p>  tag to end of string
+  quoteString += '</p>';
     //used console.log to confirm quoteString is assembling correctly - passed
     //console.log("Current: " + quoteString);
-  
-    // set the innerHTML of the quote-box div to equal the complete HTML string
-    document.getElementById('quote-box').innerHTML = quoteString; 
-    return quoteString;
-  }
-   printQuote();
+  // set the innerHTML of the quote-box div to equal the complete HTML string
+  document.getElementById('quote-box').innerHTML = quoteString; 
+  return quoteString;
+}
+  // printQuote();
   // console.log(printQuote(quotes)); -  tested printQuote Function using console.log - passsed
 
 
-  // BACKGROUND COLOR: function to change background color on click of button
-function changeBkgdColor () {
-  //used Math.floor(Math.random) from a range of 0 - 256 to generage each of the colors for RGB color set up
-  let x = Math.floor(Math.random() * 256);
-  let y = Math.floor(Math.random() * 256);
-  let z = Math.floor(Math.random() * 256);
-  //created a variable string for the new RGB color for the body color
-  let newBkgdColor = "rgb(" + x + "," + y + "," + z + ")" 
-  //applied the new color to the specific CSS Id element for the body background color;
-  document.body.style.backgroundColor = newBkgdColor;
-  console.log(newBkgdColor);
-}
+
+
+
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
-document.getElementById('load-quote').addEventListener("click", changeBkgdColor, false);
-//adding an event listenter to change the color of the background on the page on with new changeBackground function when button clicked
+
